095bfc090076fd8c285e0922e2c7a19e95703862
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-10-29 17:21:06 +0700

    fix: add custom type declaration for formidable

--- server/api/types/formidable.d.ts
+++ server/api/types/formidable.d.ts
@@ -1,2 +1,34 @@
-declare module "formidable";
-s;
+declare module "formidable" {
+  import type { IncomingMessage } from "http";
+
+  export interface File {
+    originalFilename?: string | null;
+    filepath: string;
+    mimetype?: string | null;
+    size: number;
+  }
+
+  export interface Fields {
+    [key: string]: any;
+  }
+
+  export interface Files {
+    [key: string]: File | File[];
+  }
+
+  export interface IncomingFormOptions {
+    uploadDir?: string;
+    keepExtensions?: boolean;
+    maxFileSize?: number;
+    multiples?: boolean;
+  }
+
+  export class IncomingForm {
+    constructor(options?: IncomingFormOptions);
+    parse(req: IncomingMessage): Promise<[Fields, Files]>;
+  }
+
+  export function formidable(
+    options?: IncomingFormOptions
+  ): IncomingForm;
+}
